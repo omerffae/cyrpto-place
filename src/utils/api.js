0599bcb863dd2ab043d4558f5eb4f3f802d90878
Ömer <omerekim825@gmail.com>
2025-03-09 02:13:46 +0300

    READMe.md Update

--- src/utils/api.js
+++ src/utils/api.js
@@ -4,7 +4,7 @@ const api = axios.create({
   baseURL: "https://api.coingecko.com/api/v3",
   headers: {
     accept: "application/json",
-    "x-cg-demo-api-key": "YOUR_API-KEY",
+    "x-cg-demo-api-key": "YOUR_API_KEY",
   },
 });
 
