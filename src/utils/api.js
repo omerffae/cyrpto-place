d32a4563fd84e3984dd9930f3a5cf56eeecbf26b
Ömer <omerekim825@gmail.com>
2025-03-09 02:07:04 +0300

    Create repo

---
+++ src/utils/api.js
@@ -0,0 +1,11 @@
+import axios from "axios";
+
+const api = axios.create({
+  baseURL: "https://api.coingecko.com/api/v3",
+  headers: {
+    accept: "application/json",
+    "x-cg-demo-api-key": "YOUR_API-KEY",
+  },
+});
+
+export default api;
